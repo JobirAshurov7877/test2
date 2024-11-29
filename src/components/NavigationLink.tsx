e2345c6b59e330ed687bcd893639d03749c49b85
Jobir Ashurov <jobirashurov8@gmail.com>
2024-11-29 09:52:25 +0500

    add new changes

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import { useSelectedLayoutSegment } from "next/navigation";
 import { ComponentProps } from "react";
 import { Link } from "@/i18n/routing";
+import { useLocale } from "next-intl";
 
 interface NavigationLinkProps extends ComponentProps<typeof Link> {
   href: string;
@@ -22,10 +23,11 @@ export default function NavigationLink({
   const segment = useSelectedLayoutSegment();
   const currentPath = segment ? `/${segment}` : "/";
   const isActive = currentPath === href;
+  const local = useLocale();
 
   return (
     <Link
-      href={`https://varpet.com${href}`}
+      href={`https://varpet.com/${local}${href}`}
       prefetch={prefetch}
       className={clsx(
         "transition-colors duration-200",
